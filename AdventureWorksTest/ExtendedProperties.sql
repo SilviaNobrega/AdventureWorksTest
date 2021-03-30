5c26e553194db62abf36ed904968770ed5430a5a
Silvia Nóbrega <silvia.nobrega@gmail.com>
2021-03-30 13:13:45 -0300

    Testing 1

---
+++ AdventureWorksTest/ExtendedProperties.sql
@@ -0,0 +1,6 @@
+﻿EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'AdventureWorks 2016 Sample OLTP Database';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Input parameter for the table value function ufnGetContactInformation. Enter a valid PersonID from the Person.Contact table.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'FUNCTION', @level1name = N'ufnGetContactInformation', @level2type = N'PARAMETER', @level2name = N'@PersonID';
+
---
+++ AdventureWorksTest/Sales/Tables/Store.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE [Sales].[Store] (
+    [BusinessEntityID] INT                                                NOT NULL,
+    [Name]             [dbo].[Name]                                       NOT NULL,
+    [SalesPersonID]    INT                                                NULL,
+    [Demographics]     XML(CONTENT [Sales].[StoreSurveySchemaCollection]) NULL,
+    [rowguid]          UNIQUEIDENTIFIER                                   CONSTRAINT [DF_Store_rowguid] DEFAULT (newid()) ROWGUIDCOL NOT NULL,
+    [ModifiedDate]     DATETIME                                           CONSTRAINT [DF_Store_ModifiedDate] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_Store_BusinessEntityID] PRIMARY KEY CLUSTERED ([BusinessEntityID] ASC),
+    CONSTRAINT [FK_Store_BusinessEntity_BusinessEntityID] FOREIGN KEY ([BusinessEntityID]) REFERENCES [Person].[BusinessEntity] ([BusinessEntityID]),
+    CONSTRAINT [FK_Store_SalesPerson_SalesPersonID] FOREIGN KEY ([SalesPersonID]) REFERENCES [Sales].[SalesPerson] ([BusinessEntityID])
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [AK_Store_rowguid]
+    ON [Sales].[Store]([rowguid] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Store_SalesPersonID]
+    ON [Sales].[Store]([SalesPersonID] ASC);
+
+
+GO
+CREATE PRIMARY XML INDEX [PXML_Store_Demographics]
+    ON [Sales].[Store]([Demographics])
+    WITH (PAD_INDEX = OFF);
+
